967e0dbe2eef5dea0dcae80d866dde57b8bbc127
FrontlineViewKaiser <jonas.kratzenberg@web.de>
2023-11-22 16:04:12 +0100

    portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,31 +11,24 @@ export class PortfolioComponent {
   projects = [
     new ProjectClass(
       'Join',
-      'Kanban-inspired task-management system. Create, assign and edit tasks. With dynamic contacts and drag-drop function on computer and mobile',
-      ['JS', 'CSS', 'HTML', 'API'],
+      'Kanban-inspired task-management system hooked up to a fully functioning backend. Create, assign and edit tasks. With dynamic contacts and drag-drop function on computer and mobile',
+      ['JS |', 'CSS |', 'HTML |', 'API |', 'SCRUM'],
       '/assets/img/kaiser.jpg',
       'https://github.com/FrontlineViewKaiser/ringoffire'
     ),
     new ProjectClass(
-      'Join',
-      'Kanban-inspired task-management system. Create, assign and edit tasks. With dynamic contacts and drag-drop function on computer and mobile',
-      ['JS', 'CSS', 'HTML', 'API'],
+      'Pokedex',
+      'A dynamic and colorful PokedDex based on the free PokeAPI. Fully responsive on every device',
+      ['JS |', 'CSS |', 'HTML |', 'API'],
       '/assets/img/kaiser.jpg',
-      'https://github.com/FrontlineViewKaiser/ringoffire'
+      'https://github.com/FrontlineViewKaiser/PokeDex'
     ),
     new ProjectClass(
-      'Join',
-      'Kanban-inspired task-management system. Create, assign and edit tasks. With dynamic contacts and drag-drop function on computer and mobile',
-      ['JS', 'CSS', 'HTML', 'API'],
+      'Sharkie',
+      'A Videogame done entrely through JavaScript, comes with music, touch-controls and of course a final Boss-Fight',
+      ['JS |', 'CSS |', 'HTML |', 'Git'],
       '/assets/img/kaiser.jpg',
-      'https://github.com/FrontlineViewKaiser/ringoffire'
-    ),
-    new ProjectClass(
-      'Join',
-      'Kanban-inspired task-management system. Create, assign and edit tasks. With dynamic contacts and drag-drop function on computer and mobile',
-      ['JS', 'CSS', 'HTML', 'API'],
-      '/assets/img/kaiser.jpg',
-      'https://github.com/FrontlineViewKaiser/ringoffire'
+      'https://github.com/FrontlineViewKaiser/Sharkie-Videogame-with-JS'
     ),
   ]
 
