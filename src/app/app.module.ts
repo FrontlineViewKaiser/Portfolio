e5eeed61c5deb0c2c1976e7ac0ac9e55f576726a
FrontlineViewKaiser <jonas.kratzenberg@web.de>
2023-11-24 21:01:01 +0100

    max- width

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ATFComponent } from './atf/atf.component';
@@ -40,7 +40,7 @@ import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
